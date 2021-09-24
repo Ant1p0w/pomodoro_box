2a4f21ad449a912a1738caae904976d933bdde94
antipov.ma <antipov.ma@sberleasing.ru>
2021-09-24 15:46:48 +0300

    add TaskMenu

--- src/components/TasksListContainer/TaskList/TaskList.tsx
+++ src/components/TasksListContainer/TaskList/TaskList.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useAppSelector} from '../../../hooks'
 import {TTask} from "../../../features/tasks/tasksSlice";
 import {TaskListItem} from "./TaskListItem";
 
 export function TaskList() {
     const tasksList = useAppSelector(state => state.tasks.items);
+    const [isMenuShownId, setIsMenuShownId] = useState('');
+
+    const showMenu = (uid: string) => {
+        if(uid === isMenuShownId){
+            setIsMenuShownId('');
+        }else{
+            setIsMenuShownId(uid);
+        }
+    }
 
     return (
         <ul className={'task-list mb-6 xl:pr-40'}>
             {tasksList.map((task: TTask) => (
-                <TaskListItem task={task} key={task.uid}/>
+                <TaskListItem task={task} key={task.uid} isMenuShownId={isMenuShownId} handleShowMenu={showMenu}/>
             ))}
         </ul>
     );
--- src/components/TasksListContainer/TaskList/TaskListItem/TaskListItem.tsx
+++ src/components/TasksListContainer/TaskList/TaskListItem/TaskListItem.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {MouseEventHandler, useState} from 'react';
 import {TTask} from "../../../../features/tasks/tasksSlice";
+import {TaskMenu} from "../../TaskMenu";
 
 interface ITaskListItem {
     task: TTask,
+    isMenuShownId: string,
+    handleShowMenu: (uid: string) => void
 }
 
-export function TaskListItem({task}: ITaskListItem) {
+export function TaskListItem({task, isMenuShownId, handleShowMenu}: ITaskListItem) {
     return (
         <li className={'task-list-item border-b py-5 px-1.5 flex items-center'}>
             <div className={'border rounded-full lh-1 px-4 py-2 mr-5'}>
@@ -14,13 +17,18 @@ export function TaskListItem({task}: ITaskListItem) {
             <div>
                 {task.name}
             </div>
-            <button className={'ml-auto py-2'}>
-                <svg width="26" height="6" viewBox="0 0 26 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="3" cy="3" r="3" fill="#C4C4C4"/>
-                    <circle cx="13" cy="3" r="3" fill="#C4C4C4"/>
-                    <circle cx="23" cy="3" r="3" fill="#C4C4C4"/>
-                </svg>
-            </button>
+            <div className={'ml-auto relative'}>
+                <button onClick={() => {handleShowMenu(task.uid)}} className={'py-2'}>
+                    <svg width="26" height="6" viewBox="0 0 26 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="3" cy="3" r="3" fill="#C4C4C4"/>
+                        <circle cx="13" cy="3" r="3" fill="#C4C4C4"/>
+                        <circle cx="23" cy="3" r="3" fill="#C4C4C4"/>
+                    </svg>
+                </button>
+                {isMenuShownId === task.uid && (
+                    <TaskMenu/>
+                )}
+            </div>
         </li>
     );
 }
