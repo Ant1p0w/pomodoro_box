d63b2df1472c046f979c2174e15dbd0a1645a278
antipov.ma <antipov.ma@sberleasing.ru>
2021-10-27 12:38:34 +0300

    add config container

--- src/features/tasks/configSlice.ts
+++ src/features/tasks/configSlice.ts
@@ -18,10 +18,25 @@ const initialConfigState: IConfigState = {
 }
 
 export const configSlice = createSlice({
-    name: 'tasks',
+    name: 'config',
     initialState: initialConfigState,
-    reducers: {}
+    reducers: {
+        setStatePomodoroInMin: (state, action: PayloadAction<number>) => {
+            state.pomodoroInMin = action.payload;
+        },
+        setStateSmallBreakTime: (state, action: PayloadAction<number>) => {
+            state.smallBreakTime = action.payload;
+        },
+        setStateLargeBreakTime: (state, action: PayloadAction<number>) => {
+            state.largeBreakTime = action.payload;
+        },
+        setStateTheme: (state, action: PayloadAction<number>) => {
+            state.theme = action.payload;
+        },
+    }
 })
+
+export const {setStatePomodoroInMin, setStateSmallBreakTime, setStateLargeBreakTime, setStateTheme} = configSlice.actions
 
 export const config = (state: RootState) => state.config
 
--- src/pages/ConfigPage/ConfigPage.tsx
+++ src/pages/ConfigPage/ConfigPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {ConfigContainer} from "../../components/ConfigContainer";
 
 export function ConfigPage() {
     return (
         <main>
-            123
+            <ConfigContainer/>
         </main>
     );
 }
